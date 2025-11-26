d9ea4494e49c226aa8e6e9a358acb16469f5227b
Kristoffer Carlsson <kcarlsson89@gmail.com>
2025-11-26 10:51:53 +0100

    improve chunk size picking

--- src/HyperHessians.jl
+++ src/HyperHessians.jl
@@ -166,18 +166,20 @@ end
 
 struct Chunk{N} end
 
-function Chunk(input_length::Integer)
-    return Chunk{pickchunksize(input_length)}()
+function Chunk(input_length::Integer, ::Type{T} = Float64) where {T}
+    return Chunk{pickchunksize(input_length, T)}()
 end
 
-Chunk(x::AbstractArray) = Chunk(length(x))
+Chunk(x::AbstractArray) = Chunk(length(x), eltype(x))
 
-# TODO: More testing needed here.
-function pickchunksize(input_length)
-    input_length == 1 && return 1
-    input_length == 2 && return 2
-    3 <= input_length <= 4 && return 4 # 3 seems to be very bad for some reason
-    return 8
+maxchunksize(::Type{Float64}) = 4
+maxchunksize(::Type{Float32}) = 8
+maxchunksize(::Type{T}) where {T} = 4
+
+function pickchunksize(input_length, ::Type{T}) where {T}
+    max_size = maxchunksize(T)
+    input_length <= max_size && return input_length
+    return max_size
 end
 
 chunksize(::Chunk{N}) where {N} = N::Int
