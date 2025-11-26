16b4544c6a08e3e5e98b9af942f5e15711502402
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2025-11-26 18:16:18 -0300

    start of logexpfunctions extension

--- test/rules_tests.jl
+++ test/rules_tests.jl
@@ -78,3 +78,32 @@ end
     @test Tuple(res.ϵ2) == Tuple(expected.ϵ2)
     @test Tuple(res.ϵ12[1]) == Tuple(expected.ϵ12[1])
 end
+
+@testset "LogExpFunctions.jl rule derivatives vs ForwardDiff" begin
+    seed = Vec{1, Float64}((1.0,))
+    zero_seed = Vec{1, Float64}((0.0,))
+    xs = Dict(
+        :xlogx => 0.4,
+        :logistic => 0.5,
+        :logit => 0.6,
+        :log1psq => 0.7,
+        :log1pexp => -0.1,
+        :log1mexp => -1.1,
+        :log2mexp => -1.2,
+        :logexpm1 => 1.2,
+        :log1pmx => 1.3,
+        :logmxp1 => 1.4,
+    )
+    Ext = Base.get_extension(HyperHessians,:HyperHessiansLogExpFunctionsExt)
+    for (fsym, _, _) in Ext.LOGEXPFUNCTIONS_DIFF_RULES
+        x = xs[fsym]
+        f = getfield(Base, fsym)
+        h = HyperDual(x, seed, seed, (zero_seed,))
+        res = f(h)
+        fd1 = ForwardDiff.derivative(f, x)
+        fd2 = ForwardDiff.derivative(z -> ForwardDiff.derivative(f, z), x)
+        @test res.ϵ1[1] ≈ fd1 atol = 1.0e-10 rtol = 1.0e-8
+        @test res.ϵ2[1] ≈ fd1 atol = 1.0e-10 rtol = 1.0e-8
+        @test res.ϵ12[1][1] ≈ fd2 atol = 1.0e-9 rtol = 1.0e-7
+    end
+end
