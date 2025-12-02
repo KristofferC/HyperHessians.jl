c05d586d381bc3d5b7ccb5f48fd6fb8d2cd3e036
Kristoffer Carlsson <kcarlsson89@gmail.com>
2025-12-02 16:51:38 +0100

    remove SIMD stuff (#38)
    Co-authored-by: KristofferC <kristoffer.carlsson@juliacomputing.com>

--- src/HyperHessians.jl
+++ src/HyperHessians.jl
@@ -1,14 +1,5 @@
 module HyperHessians
 
-# Allows using `SIMD.Vec` for partials but that doesn't seem to be faster in practice
-# You need to add SIMD to the package for this to work.
-const USE_SIMD = false
-
-if USE_SIMD
-    using SIMD: SIMD, Vec
-end
-
-
 if VERSION >= v"1.11.0-"
     eval(Meta.parse("public hessian, hessian!, hessiangradvalue, hessiangradvalue!, hvp, hvp!, hvpgrad, hvpgrad!, HessianConfig, DirectionalHVPConfig, Chunk"))
 end
@@ -17,9 +8,6 @@ end
 using CommonSubexpressions: cse, binarize
 
 include("hyperdual.jl")
-if USE_SIMD
-    include("simd_ops.jl")
-end
 include("rules.jl")
 include("chunks.jl")
 include("hessian.jl")
